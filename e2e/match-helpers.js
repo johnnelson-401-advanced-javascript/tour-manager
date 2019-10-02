0e811511c32a0c77918507247980b1b26b7af287
JOHN NELSON <JohnNelson4850@gmail.com>
2019-10-01 17:26:39 -0700

    tour model and routes passes

--- e2e/match-helpers.js
+++ e2e/match-helpers.js
@@ -5,7 +5,14 @@ const matchMongoId = {
   _id: expect.stringMatching(mongoId)
 };
 
+const mongoDate = /^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(\\.[0-9]+)?(Z)?/i;
+const matchMongoDate = {
+  launchDate: expect.stringMatching(mongoDate)
+};
 module.exports = {
   mongoId,
-  matchMongoId
+  matchMongoId,
+  mongoDate,
+  matchMongoDate
+
 };
